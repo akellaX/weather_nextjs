5a7d426780c47348d876bb11848f12aadc618cc7
anna <dodonova.anna76@gmail.com>
2023-07-14 03:33:00 +0200

    deleted mock server

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import styles from './page.module.css';
 import { WeatherData } from '@/types/WeatherDataServerResponse';
 import OverviewCard from '@/app/_components/OverviewCard/OverviewCard';
 import { prepareOverviewCardData } from '@/app/_components/OverviewCard/utils/prepareOverviewCardData';
+import * as weatherDataJson from '@/public/mocks/weatherApi.json';
 
-async function getWeatherData(): Promise<WeatherData> {
-    const response = await fetch('http://localhost:3000/mocks/weatherApi.json');
-    return await response.json();
-}
+// async function getWeatherData(): Promise<WeatherData> {
+//     const response = await fetch('http://localhost:3000/mocks/weatherApi.json');
+//     return await response.json();
+// }
 
 export default async function Home() {
-    const weatherData = await getWeatherData();
+    // const weatherData = await getWeatherData();
+    const weatherData = weatherDataJson as unknown as WeatherData;
     return (
         <main className={styles.main}>
             <OverviewCard
